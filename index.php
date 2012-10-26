ab5240408a8a28621a2dc0f3a34892b65ce56c72
Marc Taillefer <marctai@gmail.com>
2012-10-26 11:26:04 +0200

    inital files

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+spl_autoload_register(function($class_name) {
+    $file = 'classes/controllers/' . $class_name . '.php';
+    if (file_exists($file)) {
+        require_once($file);
+        return true;
+    }
+    $file = 'classes/models/' . $class_name . '.php';
+    if (file_exists($file)) {
+        require_once($file);
+        return true;
+    }
+    $file = 'classes/library/' . $class_name . '.php';
+    if (file_exists($file)) {
+        require_once($file);
+        return true;
+    }
+    return false;
+});
+
+
+$request = new Request();
+// echo '<pre>';
+// print_r($_SERVER);
+// echo '<br />';
+// print_r($request->url_elements);
+// echo '</pre><br />';
+// $e = class_exists('Controller');
+// die($e);
+
+// Route the request to the right place
+$controller_name = ucfirst($request->url_elements[0]) . 'Controller';
+if (class_exists($controller_name)) {
+	$controller = new $controller_name();
+	$action_name = strtolower($request->verb) . 'Action';
+	$result = $controller->$action_name($request);
+	print_r($result);
+} else {
+    echo 'controller finns icke';
+}
