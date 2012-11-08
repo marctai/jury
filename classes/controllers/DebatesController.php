d3e8efa793dc76dac3fc5c6e577afd4c10f5254a
Marc Taillefer <marctai@gmail.com>
2012-11-08 12:12:11 +0100

    fix the routing system

--- classes/controllers/DebatesController.php
+++ classes/controllers/DebatesController.php
@@ -2,44 +2,80 @@
 
 class DebatesController extends MyController
 {
-	public function getAction($request)
-	{
-		$allowed = array('open', 'pending', 'finished');
-		$data = '';
+	protected $model;
 
-		if (isset($request->url_elements[1]) && !empty($request->url_elements[1])) {
-				$dir = $request->url_elements[1];
+	public function __construct() 
+	{
+        $this->model = new DebatesModel();
+        return true;
+	}
+
+    protected function routeActions($request, $method, $allowed_dirs)
+    {
+    	$data = '';
+
+    	if (isset($request->url_elements[1]) && !empty($request->url_elements[1])) {
+			$dir = $request->url_elements[1];
 			
-			if (in_array($dir, $allowed)) {
-				$params = array_slice($request->url_elements, 2);
-				$method = 'get' . ucfirst($dir);
-				$data = $this->$method($params);
+			if (in_array($dir, $allowed_dirs)) {
+				$dirs = array_slice($request->url_elements, 2);
+				$action = $method . ucfirst($dir);
+				$data = $this->$action($dirs, $request->parameters);
 			} else {
 				throw new Exception("Couldn't find " . $dir);
 			}
 		}
 		return $data;
+    }
+
+
+	public function getAction($request)
+	{
+		$allowed_dirs = array('open', 'pending', 'finished');
+		
+		$data = $this->routeActions($request, 'get', $allowed_dirs);
+
+		return $data;
 	}
 
-	public function postAction($request)
+	public function putAction($request)
 	{
-		// ...
+		$allowed_dirs = array('open');
+		
+		$data = $this->routeActions($request, 'put', $allowed_dirs);
+
+		return $data;
 	}
 
-	protected function getOpen($params)
+	protected function getOpen($dirs, $params)
 	{
-		$allowed = array('random');
-
-		if (!empty($params) && !$params[0] == '') {
+		$allowed_dirs = array('random');
+        
+		if (!empty($dirs) && !$dirs[0] == '') {
 			
-			if (in_array($params[0], $allowed)) {
+			if (in_array($dirs[0], $allowed_dirs)) {
 				// open/random stuff!
-				return 'open/' . $params[0];
+				return 'open/' . $dirs[0];
 			} else {
-				throw new Exception("Couldn't find " . $params[0]);
+				throw new Exception("Couldn't find " . $dirs[0]);
 			}
 		}
-		return 'open';
+		return $this->model->getOpen();
+	}
+
+	protected function putOpen($dirs, $params)
+	{
+		if (isset($dirs[0]) && ctype_digit($dirs[0])) {
+           
+            if (isset($params['arg']) && isset($params['stance'])) {
+		        // TODO: Some kind of validation of params
+	    	    print_r($params);
+	    	    $this->model->putOpen($dirs[0], $params['arg'], $params['stance']);
+	    	} else {
+	    		throw new Exception('Missing parameters');
+	    	}
+		}
+
 	}
 
 	protected function getPending($params)
